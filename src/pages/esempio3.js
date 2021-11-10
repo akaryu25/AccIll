d8236358bd01d343809593ca7c08b37304da28c6
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-10 17:49:29 +0100

    Update esempio3.js

--- src/pages/esempio3.js
+++ src/pages/esempio3.js
@@ -8,12 +8,14 @@ import Seo from "../components/seo"
 const esempio3 = () => (
   <Layout>
     <Seo title="Esempio3" />
-    <p> 
-    Elicottero rumoroso <br /> Sasso bianco <br /> Edera verde <br /> Musica soave <br /> Pittura artistica <br /> Intelligenza artificiale <br /> Onda quadra
-    </p>
-    <p>
-    mneudsirau
-    </p>
+    DF73FF
+FF2400 <br />
+C2B280 <br />
+FF00FF <br />
+D1E231 <br />
+4B0082 <br />
+FFD700 <br />
+01100100 01101001 01100110 01100110 01101001 01100011 01101001 01101100 01100101
 
     <p>
       Guarda la &nbsp; <Link to="/esempio2risolto/">soluzione</Link> 
