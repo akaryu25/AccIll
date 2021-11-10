9e31c8581a09b9679fdf29d7a2435ee2eeddcf48
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-10 17:47:44 +0100

    Create esempio3.js

---
+++ src/pages/esempio3.js
@@ -0,0 +1,25 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const esempio3 = () => (
+  <Layout>
+    <Seo title="Esempio3" />
+    <p> 
+    Elicottero rumoroso <br /> Sasso bianco <br /> Edera verde <br /> Musica soave <br /> Pittura artistica <br /> Intelligenza artificiale <br /> Onda quadra
+    </p>
+    <p>
+    mneudsirau
+    </p>
+
+    <p>
+      Guarda la &nbsp; <Link to="/esempio2risolto/">soluzione</Link> 
+    </p>
+    
+  </Layout>
+)
+
+export default esempio3
