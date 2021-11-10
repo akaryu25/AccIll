60fd95ff04bec142d9f7d39508a90eaed64a36d2
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-10 17:57:21 +0100

    Update esempio3.js

--- src/pages/esempio3.js
+++ src/pages/esempio3.js
@@ -8,14 +8,14 @@ import Seo from "../components/seo"
 const esempio3 = () => (
   <Layout>
     <Seo title="Esempio3" />
-    DF73FF
-FF2400 <br />
-C2B280 <br />
-FF00FF <br />
-D1E231 <br />
-4B0082 <br />
-FFD700 <br />
-01100100 01101001 01100110 01100110 01101001 01100011 01101001 01101100 01100101
+    DF73FF <br />
+    FF2400 <br />
+    C2B280 <br />
+    FF00FF <br />
+    D1E231 <br />
+    4B0082 <br />
+    FFD700 <br />
+    01100100 01101001 01100110 01100110 01101001 01100011 01101001 01101100 01100101
 
     <p>
       Guarda la &nbsp; <Link to="/esempio2risolto/">soluzione</Link> 
