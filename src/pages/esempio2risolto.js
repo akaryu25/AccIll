005478ccaa3d5fec880357f859cae1ba980160d3
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-10 17:16:00 +0100

    Update esempio2risolto.js

--- src/pages/esempio2risolto.js
+++ src/pages/esempio2risolto.js
@@ -9,12 +9,15 @@ const esempio2risolto = () => (
   <Layout>
     <Seo title="Esempio2risolto" />
     <p> 
-    Elicottero rumoroso <br> Sasso bianco <br> Edera verde <br> Musica soave <br> Pittura artistica <br> Intelligenza artificiale <br> Onda quadra 
+    Elicottero rumoroso <br /> Sasso bianco <br /> Edera verde <br /> Musica soave <br /> Pittura artistica <br /> Intelligenza artificiale <br /> Onda quadra 
     </p>
     <p>
     mneudsirau
     </p>
-
+    <p>Per la prima parte baster&agrave; leggere le iniziali di ogni frase ottenendo la sequenza E S E M P I O</p>
+    <p>La seconda parte invece &egrave; una serie di lettere, che raggruppate a due a due formano mn eu ds ir au. Scrivendoli in questo modo forse sono pi&ugrave; riconoscibili: Mn, Eu, Ds, Ir, Au.</p>
+    <p>Manganese, Europio, Darmstadtio, Iridio, Oro, le cui iniziali formano M E D I O.</p>
+    <p>La risposta &egrave; ESEMPIO MEDIO.</p>
     <p>
       Vai al &nbsp;<Link to="/">prossimo</Link> <br />
     </p>
