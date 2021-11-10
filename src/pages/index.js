63c0805d1899acae21fffdac7653e8b1839b715a
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-10 17:56:06 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,9 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <p>Benvenuto/a nell'Accademia Oscura.</p>
-    <p>Sono qui per guidarti ad intraprendere la strada verso l'Oscurit&agrave;. Non temere, &egrave; solo un sostantivo femminile che serve ad indicare la mancanza o insufficienza di luce. Vedi, lo scopo dell'accademia &egrave; quello di darti le conoscenze base per risolvere gli Enigmi.</p>
+    <p>Sono qui per guidarti ad intraprendere la strada verso l'Oscurit&agrave;. Non temere, &egrave; solo un sostantivo femminile che serve ad indicare la mancanza o insufficienza di luce. Vedi, lo scopo dell'accademia &egrave; quello di darti le conoscenze base per comprendere gli Enigmi.</p>
+    <br /> Gli Enigmi ti porteranno verso di me, ma solo chi sarà in grado di risolverli raggiungerà la terra promessa.
+  
     <p>Ogni Enigma &egrave; indicato con l'associazione di un nome e un aggettivo, ad esempio "Accademia Oscura"</p>
     <p>La soluzione di ogni Enigma porter&agrave; sempre a quello successivo, quindi avr&agrave; sempre questa forma.</p>
     <p><Link to="/esempio1/">Qui</Link> troverai degli esempi guidati. Prova a risolverli!</p>
