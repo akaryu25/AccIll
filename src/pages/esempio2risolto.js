b7aa87340e15ec23d6e12baf7a82865d8e5f501d
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-10 16:56:15 +0100

    Create esempio2risolto.js

---
+++ src/pages/esempio2risolto.js
@@ -0,0 +1,25 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const esempio2risolto = () => (
+  <Layout>
+    <Seo title="Esempio2risolto" />
+    <p> 
+    Elicottero rumoroso <br> Sasso bianco <br> Edera verde <br> Musica soave <br> Pittura artistica <br> Intelligenza artificiale <br> Onda quadra 
+    </p>
+    <p>
+    mneudsirau
+    </p>
+
+    <p>
+      Vai al &nbsp;<Link to="/">prossimo</Link> <br />
+    </p>
+    
+  </Layout>
+)
+
+export default esempio2risolto
