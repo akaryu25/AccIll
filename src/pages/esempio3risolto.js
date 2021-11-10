9d9ee5e82f1cb2704532297ebdb377129dec899d
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-10 18:12:33 +0100

    Update esempio3risolto.js

--- src/pages/esempio3risolto.js
+++ src/pages/esempio3risolto.js
@@ -18,7 +18,7 @@ const esempio3 = () => (
     01100100 01101001 01100110 01100110 01101001 01100011 01101001 01101100 01100101
     <p> </p>
     <p>I pi&ugrave; difficili sono solitamente quelli che contengono codici particolari che bisogna conoscere per decifrare. Ad esempio supponendo di non sapere questa codifica, baster&agrave; cercare su Google "DF73FF" e si trover&agrave; subito la corrispondenza per un colore. Questo colore &egrave; chiamato Eliotropo. La tabella dei colori &egrave; facilmente consultabile su Wikipedia, cos&igrave; come molti altri codici. Insieme a Eliotropo le iniziali degli altri colori formano la parola ESEMPIO.</p>
-    <p>Ci sono siti specializzati nella codifica/decodifica, non aver paura ad utilizzare quelli. Dovrai solo scoprire quale tipo di "cifrario" &egrave; stato usato. Il secondo indizio &egrave; un codice binario, che va tradotto in qualche modo. Ci sono siti come &nbsp <a href="https://traduttorebinario.com/">questo</a> che permettono di convertire il binario in un testo. Si ottiene cos&igrave; la parola "difficile".</p>
+    <p>Ci sono siti specializzati nella codifica/decodifica, non aver paura ad utilizzare quelli. Dovrai solo scoprire quale tipo di "cifrario" &egrave; stato usato. Il secondo indizio &egrave; un codice binario, che va tradotto in qualche modo. Ci sono siti come &nbsp;<a href="https://traduttorebinario.com/" target="_blank">questo</a> che permettono di convertire il binario in un testo. Si ottiene cos&igrave; la parola "difficile".</p>
     <p>La soluzione sar&agrave; ESEMPIO DIFFICILE.</p>
     <p>
       <Link to="/esame/">Prova ad entrare nell'accademia! </Link> 
