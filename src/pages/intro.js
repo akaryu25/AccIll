d604b22f23f39f426dbd1171af8663457ece9355
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-11 22:50:50 +0100

    Update intro.js

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -12,6 +12,7 @@ const intro = () => (
     <p>Ogni Enigma &egrave; indicato con l'associazione di un nome e un aggettivo, ad esempio "Accademia Oscura"
     <br />La soluzione di ogni Enigma porter&agrave; sempre a quello successivo, quindi avr&agrave; sempre questa forma.
     <br />Gli Enigmi saranno solo in italiano, ma ci potranno essere altre lingue che quindi dovranno essere intese come indizi.
+    <br />Per tornare alla pagina principale dell'Enigma premi sul nome nella parte rossa in alto.
     <br />Se pensi di aver trovato la soluzione, vai su &nbsp;<a ref="https://hns.to/" target="_blank"> https://hns.to/ </a> e digita nella barra in alto la soluzione senza spazi.
     <br />Per esempio se la soluzione è "Sito strano" digita "sitostrano" e premi invio. Se la risposta è corretta verrai mandato all'Enigma successivo. Altrimenti riceverai un errore oppure, in un caso molto raro potresti finire in qualche altro sito.&nbsp;<Link to="/spiegazione/">Perchè questo?</Link> 
     <br />Apri &nbsp;<Link to="/spiegazione/">qui</Link> se vuoi altre info sul sistema utilizzato, o se volete semplificare la procedura.
@@ -19,7 +20,7 @@ const intro = () => (
     <br />Non ci sono altre regole, tutto è permesso.
     </p>
     <p>
-      Buona fortuna! &nbsp;<Link to="/spiegazione/">Procedi con il tutorial</Link>.
+      Buona fortuna! &nbsp;<Link to="/esempio1/">Procedi con il tutorial</Link>.
     </p>
     
   </Layout>
