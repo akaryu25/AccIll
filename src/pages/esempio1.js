c28802b4ea5fdaf47f79faee30e49750dee02c35
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-11 22:29:00 +0100

    Update esempio1.js

--- src/pages/esempio1.js
+++ src/pages/esempio1.js
@@ -8,9 +8,7 @@ import Seo from "../components/seo"
 const esempio1 = () => (
   <Layout>
     <Seo title="Esempio1" />
-    <p>Ogni Enigma &egrave; indicato con l'associazione di un nome e un aggettivo, ad esempio "Accademia Oscura"
-    <br />La soluzione di ogni Enigma porter&agrave; sempre a quello successivo, quindi avr&agrave; sempre questa forma.</p>
-  
+   
     <p>Quasi sEmpre il titolo dell'enigma &egrave; un'informazione indiSpEnsabile per risolverlo, quindi Mi raccomando non dimenticartene! In questo caso il titolo &egrave; "Accademia Oscura" e non aiuta in alcun modo a trovare la soluzione di questi esemPI, ma nOn sar&agrave; sempre cos&igrave;.</p>
     <p>Alcuni codici saranno facili da decifrare, altri meno. Prima di perdere ogni speranza, prova a cercare il codice su Google. Molto probabilmente non troverai nulla ma tentar non nuoce, soprattutto se hai gi&agrave; provato vari modi per decifrarlo e non hanno portato risultati. In quel caso potrebbe proprio essere che il codice sia un'etichetta di qualche tipo e lo troverai subito sul motore di ricerca.</p>
     <StaticImage
