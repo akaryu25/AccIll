f3b9594fb35f7fa98b3adbb5ee43feb2827b364e
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-11 23:00:41 +0100

    Update intro.js

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -10,19 +10,18 @@ const intro = () => (
     <Seo title="Intro" />
     <p> Prima di iniziare ecco le regole del gioco: </p>
     <p>Ogni Enigma &egrave; indicato con l'associazione di un nome e un aggettivo, ad esempio "Accademia Oscura".
-    <br />La soluzione di ogni Enigma porter&agrave; sempre a quello successivo, quindi avr&agrave; sempre questa forma.
-    <br />Gli Enigmi saranno solo in italiano, ma ci potranno essere altre lingue che quindi dovranno essere intese come indizi.
+    <br />La soluzione di ogni Enigma porter&agrave; sempre a quello successivo, quindi avr&agrave; ancora questa forma.
+    <br />Gli Enigmi saranno solo in italiano, ma potranno comparire altre lingue che quindi dovranno essere intese come indizi.
     <br />Per tornare alla pagina principale dell'Enigma premi sul nome nella parte rossa in alto.
     <br />Se pensi di aver trovato la soluzione, vai su &nbsp;<a ref="https://hns.to/" target="_blank"> https://hns.to/ </a> e digita nella barra in alto la soluzione senza spazi.
     <br />Per esempio se la soluzione è "Sito strano" digita "sitostrano" e premi invio. Se la risposta è corretta verrai mandato all'Enigma successivo. Altrimenti riceverai un errore oppure, in un caso molto raro potresti finire in qualche altro sito.&nbsp;<Link to="/spiegazione/">Perchè questo?</Link> 
-    <br />Apri &nbsp;<Link to="/spiegazione/">qui</Link> se vuoi altre info sul sistema utilizzato, o se volete semplificare la procedura.
     <br />Non verranno dati indizi finchè non arriverete alla terra promessa. Se rimanete bloccati, provate a chiedere ad altre persone di aiutarvi. L'unione fa la forza!
-    <br />Non ci sono altre regole, tutto è permesso.
+    <br />Non ci sono altre regole.
     </p>
     <p>
       Buona fortuna! &nbsp;<Link to="/esempio1/">Procedi con il tutorial</Link>.
     </p>
-    
+    <p> Apri &nbsp;<Link to="/spiegazione/">qui</Link> se vuoi altre info sul sistema utilizzato per verificare le risposte, o se volete semplificare la procedura.</p>
   </Layout>
 )
 
