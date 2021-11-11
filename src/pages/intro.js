f50eaaeb905c1ac752c34b016374ba32962befce
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-11 23:01:11 +0100

    Update intro.js

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -13,7 +13,7 @@ const intro = () => (
     <br />La soluzione di ogni Enigma porter&agrave; sempre a quello successivo, quindi avr&agrave; ancora questa forma.
     <br />Gli Enigmi saranno solo in italiano, ma potranno comparire altre lingue che quindi dovranno essere intese come indizi.
     <br />Per tornare alla pagina principale dell'Enigma premi sul nome nella parte rossa in alto.
-    <br />Se pensi di aver trovato la soluzione, vai su &nbsp;<a ref="https://hns.to/" target="_blank"> https://hns.to/ </a> e digita nella barra in alto la soluzione senza spazi.
+    <br />Se pensi di aver trovato la soluzione, vai su &nbsp;<a href="https://hns.to/" target="_blank"> https://hns.to/ </a> e digita nella barra in alto la soluzione senza spazi.
     <br />Per esempio se la soluzione è "Sito strano" digita "sitostrano" e premi invio. Se la risposta è corretta verrai mandato all'Enigma successivo. Altrimenti riceverai un errore oppure, in un caso molto raro potresti finire in qualche altro sito.&nbsp;<Link to="/spiegazione/">Perchè questo?</Link> 
     <br />Non verranno dati indizi finchè non arriverete alla terra promessa. Se rimanete bloccati, provate a chiedere ad altre persone di aiutarvi. L'unione fa la forza!
     <br />Non ci sono altre regole.
