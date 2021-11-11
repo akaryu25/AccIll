f2b714c44b164ce2bcd7e8fd8d2815a924ce46c2
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-11 21:50:33 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,11 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <p>Benvenuto/a nell'Accademia Oscura.</p>
-    <p>Sono qui per guidarti nell' intraprendere la strada verso l'Oscurit&agrave;. Non temere, &egrave; solo un sostantivo femminile che serve ad indicare la mancanza o insufficienza di luce. Vedi, lo scopo dell'accademia &egrave; quello di darti le conoscenze base per comprendere gli Enigmi.</p>
-    <br /> Gli Enigmi ti porteranno verso di me, ma solo chi sarà in grado di risolverli raggiungerà la terra promessa.
+    <p>Sono qui per guidarti nell' intraprendere la strada verso l'Oscurit&agrave;. Non temere, &egrave; solo un sostantivo femminile che serve ad indicare la mancanza o insufficienza di luce. Vedi, lo scopo dell'accademia &egrave; quello di darti le conoscenze base per comprendere gli Enigmi.
+    <br />Gli Enigmi ti porteranno in un posto buio, e servirà orientarsi tramite le proprie abilità. Solo chi sarà in grado di risolverli raggiungerà la terra promessa.</p>
   
-    <p>Ogni Enigma &egrave; indicato con l'associazione di un nome e un aggettivo, ad esempio "Accademia Oscura"</p>
-    <p>La soluzione di ogni Enigma porter&agrave; sempre a quello successivo, quindi avr&agrave; sempre questa forma.</p>
-    <p><Link to="/esempio1/">Qui</Link> troverai degli esempi guidati. Prova a risolverli!</p>
-    <p> </p>
+    <p>Tranquillo/a, per iniziare &nbsp;<Link to="/esempio1/">qui</Link> troverai degli esempi guidati. Prova a risolverli!</p>
+    Se invece pensi di essere già pronto &nbsp;<Link to="/test/">questo</Link> è il test per entrare nell'accademia e iniziare a risolvere gli enigmi.
  
   </Layout>
 )
