fa68fea7b0fbe0f4d282a4fd35b5264b27232c1d
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-12 12:10:23 +0100

    Create esame.js

---
+++ src/pages/esame.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const esame = () => (
+  <Layout>
+    <Seo title="Esame" />
+    <p>Oltre che solitario è anche... </p>
+  <p></p>
+    <StaticImage
+      src="../images/esame.png"
+      width={600}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="A test to prove you are worthy"
+      style={{ marginBottom: `1.45rem` }}
+    />
+   
+    
+  </Layout>
+)
+
+export default esame
