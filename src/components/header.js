3f8021828f497aea13a59fab04f7194ce98b2d0b
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-12 20:44:51 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#ff4444`,
+      background: `#ffd440`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -13,14 +13,16 @@ const Header = ({ siteTitle }) => (
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `0.5rem 0.5rem`,
+        justifyContent: `center`,
+        textAlign: `center`,
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#282828`,
             textDecoration: `none`,
           }}
         >
