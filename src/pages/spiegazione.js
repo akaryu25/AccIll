c662e99f430c2036b8524965610a2a433ecec73b
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-12 12:26:09 +0100

    Update spiegazione.js

--- src/pages/spiegazione.js
+++ src/pages/spiegazione.js
@@ -11,8 +11,8 @@ const spiegazione = () => (
     Ogni Enigma è in realtà un sito web statico.
     <br />L'indirizzo del sito è ".accademiaoscura", molto più comodo di "accademiaoscura.com".
     <br />Per fare questo però è necessario utilizzare DNS compatibili, tipo impostando il DNS primario 103.196.38.38 e secondario 103.196.38.39
-    <br />Facendo ciò ci si affida a quel servizio e bisogna sperare che non utilizzino male i nostri dati. Molto meglio utilizzare questo sito che farà da ponte.
-    <br />Altrimenti, si possono usare browser compatibili tipo <a href="https://www.pumabrowser.com/" target="_blank">https://www.pumabrowser.com/ </a> o l'estensione per chrome
+    <br />Facendo ciò ci si affida a quel servizio e bisogna sperare che non utilizzino male i nostri dati. Molto meglio utilizzare <a href="https://hns.to/" target="_blank"> questo </a>  sito che farà da ponte.
+    <br />Altrimenti, si possono usare browser compatibili tipo <a href="https://www.pumabrowser.com/" target="_blank">https://www.pumabrowser.com/ </a> o l'estensione per chrome <a href="https://chrome.google.com/webstore/detail/bob-extension/ogcmjchbmdichlfelhmceldndgmgpcem" target="_blank">Bob Extension </a>
     <br />
   </Layout>
 )
