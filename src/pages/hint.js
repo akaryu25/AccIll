a58389abd2c540ff0e756956a0c87c44a189fa0e
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-12 21:03:34 +0100

    Update hint.js

--- src/pages/hint.js
+++ src/pages/hint.js
@@ -12,7 +12,7 @@ const hint = () => (
     Una volta decifrata l'immagine capirai in che contesto va ricercata la frase.
     <br /> Visto che questo è il tuo primo test, ho deciso di darti un aiutino. Sarà l'ultimo ricorda!
     <br /> Può capitare che la risoluzione necessiti di informazioni che non hai, e che dovranno essere cercate.
-    <br /> Questo vuol dire che in alcuni casi conviene fare un'attenta ricerca su Wikipedia, Google o qualche sito specializzato.
+    <br /> Questo vuol dire che in alcuni casi conviene fare un'attenta ricerca su Wikipedia o Google per esempio.
     <br /> In questo caso troverai che le esatte parole nella frase continuano con la soluzione dell'Enigma... Se sei nel posto giusto ovviamente!
   </p>
   <p> <Link to="/esame/">Torna indietro</Link></p>
