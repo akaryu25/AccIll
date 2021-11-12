30a7bc6686059e875fd1cdbb6dfa95ca6e3e6fb5
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-12 02:22:40 +0100

    Update esame.js

--- src/pages/esame.js
+++ src/pages/esame.js
@@ -9,7 +9,7 @@ const esame = () => (
   <Layout>
     <Seo title="Esame" />
     <p>Oltre che solitario è anche... </p>
-  
+    
     <StaticImage
       src="../images/esame.png"
       width={600}
@@ -18,10 +18,10 @@ const esame = () => (
       alt="A test to prove you are worthy"
       style={{ marginBottom: `1.45rem` }}
     />
-   
     
+    <p> <br /> <br /> </p>
     <a href="https://hns.to/" target="_blank"> Ho risolto. </a>
-    PS: ricordati la pagina per immettere le soluzioni nei preferiti, così non perdi la via.
+    <p>PS: ricordati la pagina per immettere le soluzioni nei preferiti, così non perdi la via.</p>
   </Layout>
 )
 
