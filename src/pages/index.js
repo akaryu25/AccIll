4e7d5fd6715016b888636e5dfb2c5e355dbbaa2d
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-13 22:17:00 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,13 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <p>Benvenuto/a nell'Accademia Oscura.</p>
-    <p>Sono qui per guidarti nell' intraprendere la strada verso l'Oscurit&agrave;. Non temere, serve solo ad indicare la mancanza o insufficienza di luce. Vedi, lo scopo dell'accademia &egrave; quello di darti le conoscenze base per comprendere gli Enigmi.
-    <br />Gli Enigmi ti porteranno in un posto buio, e servirà orientarsi tramite le proprie abilità. Solo chi sarà in grado di risolverli raggiungerà la terra promessa.</p>
+    <p>Sono qui per guidarti nell' intraprendere la strada verso l'Oscurit&agrave; degli Enigmi. Non temere, lo scopo dell'accademia &egrave; quello di darti le conoscenze base per comprendere gli Enigmi.
+    <br />Solo chi sarà in grado di risolverli raggiungerà la terra promessa.</p>
   
-    <p>Tranquillo/a, per iniziare &nbsp;<Link to="/intro/">qui</Link> troverai le regole e degli esempi guidati. Prova a risolverli!
+    <p>Per iniziare &nbsp;<Link to="/intro/">qui</Link> troverai le regole e degli esempi guidati. Prova a risolverli!
     </p>
   <p>
-    Se invece pensi di essere già pronto &nbsp;<Link to="/esame/">questo</Link> è il test per entrare nell'accademia e iniziare a risolvere gli enigmi.
+    Se invece pensi di essere già pronto &nbsp;<Link to="/esame/">questo</Link> è il test per entrare nell'accademia.
   </p>
   </Layout>
 )
